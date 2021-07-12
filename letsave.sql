335633fbf6d8d1d56f9a38cdc4f2fd631391b1f0
hoagnhat <hahng.nhat@gmail.com>
2021-07-12 16:41:21 +0700

    add db script

--- letsave.sql
+++ letsave.sql
@@ -4,7 +4,7 @@ CREATE TABLE `accounts` (
   `password` char(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `accounts_categories` (
   `accountId` bigint NOT NULL,
@@ -22,6 +22,13 @@ CREATE TABLE `balances` (
   CONSTRAINT `balances_ibfk_1` FOREIGN KEY (`accountId`) REFERENCES `accounts` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `categories` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` char(200) DEFAULT NULL,
+  `image` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `budgets` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `accountId` bigint NOT NULL,
@@ -38,12 +45,7 @@ CREATE TABLE `budgets` (
   CONSTRAINT `budgets_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `categories` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` char(200) DEFAULT NULL,
-  `image` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `profiles` (
   `accountId` bigint NOT NULL,
@@ -63,9 +65,16 @@ CREATE TABLE `transactions` (
   `amount` float NOT NULL,
   `categoryId` bigint NOT NULL,
   `date` date DEFAULT NULL,
+  `note` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `accountId` (`accountId`),
   KEY `transactions_ibfk_2` (`categoryId`),
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`accountId`) REFERENCES `accounts` (`id`) ON DELETE CASCADE,
   CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO categories (name, image)
+VALUES ('travel', 'https://i.ibb.co/94Y6gqR/car-48px.png'),
+('food', 'https://i.ibb.co/0rqthPZ/hamburger-48px.png'),
+('hospital', 'https://i.ibb.co/9gssnM6/hospital-3-48px.png'),
+('bank', 'https://i.ibb.co/ByGWWQF/android-48px.png');
